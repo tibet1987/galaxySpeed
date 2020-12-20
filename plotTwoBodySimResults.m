20fa79bbf1300cb254085b2936accc46e15cb1c1
Timo <thundy@web.de>
2020-12-20 17:50:25 +0100

    add plotting feature for two body dynamics

---
+++ plotTwoBodySimResults.m
@@ -0,0 +1,70 @@
+function plotTwoBodySimResults(r1,r2,varargin)
+% plotTwoBodySimResults(r1,r2)
+%
+% input:
+%  r1    - timeseries with position of mass 1
+%  r2    - timeseries with position of mass 2
+%
+% varargin [optional]
+%  'size1'      - size of mass 1
+%  'size2'      - size of mass 2
+%  'timeScale'  - scaling of time during between frames (default: 8640000)
+%
+% time series of 3d vectors r1, r2
+
+% default values
+size1 = 25;
+size2 = 15;
+timeScale = 24*3600*100;
+reps = 0;
+useNewFigure = 1;
+for k=1:2:nargin-2
+    if strcmpi(varargin{k},'size1')
+        size1 = varargin{k+1};
+    elseif strcmpi(varargin{k},'size2')
+        size2 = varargin{k+1};
+    elseif strcmpi(varargin{k},'timeScale')
+        timeScale = varargin{k+1};
+    elseif strcmpi(varargin{k},'plotTrail')
+        plotTrail = varargin{k+1};
+    end        
+
+end
+
+figure('color','k','position',[2    42   958   954])
+
+% plot initial bodies dots
+p1 = plot(r1.Data(1,1),r1.Data(1,2),'.r','MarkerSize',size1);
+hold all
+p2 = plot(r2.Data(1,1),r2.Data(1,2),'.w','MarkerSize',size2);
+if plotTrail
+    trail1 = plot(r1.Data(1,1),r1.Data(1,2),'-r','lineWidth',0.5');
+    trail2 = plot(r2.Data(1,1),r2.Data(1,2),'-w','lineWidth',0.5');
+end
+drawnow
+
+set(gca,'color','k')
+plotScale = 1.1;
+xmin = min([r1.Data(:,1);r2.Data(:,1)]) * plotScale;
+xmax = max([r1.Data(:,1);r2.Data(:,1)]) * plotScale;
+ymin = min([r1.Data(:,2);r2.Data(:,2)]) * plotScale;
+ymax = max([r1.Data(:,2);r2.Data(:,2)]) * plotScale;
+axis equal
+xlim([xmin,xmax])
+ylim([ymin,ymax])
+timeDiff = (r1.Time(2)-r1.Time(1)) / timeScale;
+
+for k=2:numel(r1.Time)-1
+    pause( timeDiff );
+    set(p1,'XData',r1.Data(k,1),'YData',r1.Data(k,2));
+    set(p2,'XData',r2.Data(k,1),'YData',r2.Data(k,2));
+    if plotTrail
+        set(trail1,'XData',r1.Data(1:k,1),'YData',r1.Data(1:k,2));
+        set(trail2,'XData',r2.Data(1:k,1),'YData',r2.Data(1:k,2));
+    end
+    drawnow
+end
+
+
+
+end
--- sim_solarSystem.m
+++ sim_solarSystem.m
@@ -7,36 +7,42 @@ addpath functions
 %% understanding Kepler movement
 
 
-    %% drawing different Kepler orbits
-    close all
-    phi = linspace(0,2*pi,100); % linear true anomaly
-    a = 1;
-    e = 0.80;
-    plot(0,0,'.','MarkerSize',20); hold all;
-    [r] = calcKeplerRadius(phi,a,e);
-    [x,y] = pol2cart(phi,r);
-    plot(x,y,'.-')
-    axis equal
-    hold all
+%% drawing different Kepler orbits
+close all
+phi = linspace(0,2*pi,100); % linear true anomaly
+a = 1;
+e = 0.80;
+plot(0,0,'.','MarkerSize',20); hold all;
+[r] = calcKeplerRadius(phi,a,e);
+[x,y] = pol2cart(phi,r);
+plot(x,y,'.-')
+axis equal
+hold all
 
-    
-    %% movement of two bodies attacked by gravity
-    close all
-    lengthScaling = 1e-6;
-    m1 = 2e30; %kg mass of the sun
-    m2 = 1;%6e24; % mass of the earth
-    r2_init = [149e9 0, 0]';
-    r2dot_init = [0, 29.78e3,0]';
-    r1_init = -r1_init*1e-6;
-    r1dot_init = -r1dot_init*1e-6;
-    G = 6.674e-11; % m^3/(kg s^2)
-    
-    F_G = G*m1*m2/r1_init(1)^2;
-    F_z = m2*r1dot_init(2)^2 / r1_init(1);
-    
-    Tsim = 1e8;
-    %%
-%     sim('twoBodySim')
-    
+
+%% movement of two bodies attacked by gravity
+close all
+m1 = 2e30; %kg mass of the sun
+m2 = 1;%6e24; % mass of the earth
+fixPositionOfMass1 = 1;
+r2_init = [149e9 0, 0]';
+r2dot_init = [0, 29.78e3,0]';
+r1_init = -r2_init*1e-6;
+r1dot_init = -r2dot_init*1e-6;
+G = 6.674e-11; % m^3/(kg s^2)
+
+F_G = G*m1*m2/r1_init(1)^2;
+F_z = m2*r1dot_init(2)^2 / r1_init(1);
+
+Tsim = 2*365*24*3600; % 1 year
+T_sample = 24*3600; % 1 day
+
+sim('twoBodySim')
+
+%%
+close all
+plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
+                            'timeScale',T_sample*20)
+
 
 %% first test - simualte earth movement
