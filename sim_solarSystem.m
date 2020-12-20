a502a37cb152ac17fbbcb7158b0a64c2fbeedaca
Timo <thundy@web.de>
2020-12-20 15:23:22 +0100

    first running simulation
    simulation with fixed sun (inite mass assumed)
    simulation with neglected earth mass (m_earth=m2=1)

--- sim_solarSystem.m
+++ sim_solarSystem.m
@@ -22,16 +22,21 @@ addpath functions
     
     %% movement of two bodies attacked by gravity
     close all
-    m1 = 6e24; %kg
-    m2 = 1;
-    r1_init = [150e9 0]';
-    r1dot_init = [0, 29.78e3]';
-    r2_init = -r1_init;
-    r2dot_init = -r1dot_init;
+    lengthScaling = 1e-6;
+    m1 = 2e30; %kg mass of the sun
+    m2 = 1;%6e24; % mass of the earth
+    r2_init = [149e9 0, 0]';
+    r2dot_init = [0, 29.78e3,0]';
+    r1_init = -r1_init*1e-6;
+    r1dot_init = -r1dot_init*1e-6;
     G = 6.674e-11; % m^3/(kg s^2)
+    
+    F_G = G*m1*m2/r1_init(1)^2;
+    F_z = m2*r1dot_init(2)^2 / r1_init(1);
+    
     Tsim = 1e8;
     %%
-    sim('twoBodySim')
+%     sim('twoBodySim')
     
 
 %% first test - simualte earth movement
