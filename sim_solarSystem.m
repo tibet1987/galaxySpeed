07148701c783595738dbcb6d155fd1d5378613b5
Timo <thundy@web.de>
2020-12-21 10:08:02 +0100

    add eccentric orbit and docu for two body circular orbit

--- sim_solarSystem.m
+++ sim_solarSystem.m
@@ -35,6 +35,28 @@ F_G = G*m1*m2/r1_init(1)^2;
 F_z = m2*r1dot_init(2)^2 / r1_init(1);
 
 Tsim = 2*365*24*3600; % 1 year
+T_sample = 24*3600; % 2 days
+
+sim('twoBodySim')
+
+plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
+                            'timeScale',T_sample*50)
+
+%% second test - simualte eccentric orbit with neglected m2
+close all
+m1 = 2e30; %kg mass of the sun
+m2 = 1;%6e24; % mass of the earth
+fixPositionOfMass1 = 1;
+r2_init = [149e9 0, 0]';
+r2dot_init = [0, 29.78e3*0.5,0]';
+r1_init = -r2_init*1e-6;
+r1dot_init = -r2dot_init*1e-6;
+G = 6.674e-11; % m^3/(kg s^2)
+
+F_G = G*m1*m2/r1_init(1)^2;
+F_z = m2*r1dot_init(2)^2 / r1_init(1);
+
+Tsim = 2*365*24*3600; % 1 year
 T_sample = 24*3600; % 1 day
 
 sim('twoBodySim')
@@ -63,22 +85,23 @@ plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
                         
 %% movement of two bodies attacked by gravity
 close all
-m1 = 1e22; % kg, mass of body 1 (Pluto equivalent)
+G = 6.674e-11; % m^3/(kg s^2)
+m1 = 1e15; % kg, mass of body 1 (Pluto equivalent)
 m2 = m1;   % kg, mass of body 2
 fixPositionOfMass1 = 0;
-r2_init = [5e12 0, 0]';
-r2dot_init = [0, 0.01e3,0]';
+r2_init = [5e8 0, 0]';
+r2dot_init = [0, sqrt(G*m1/r2_init(1)),0]'; % 0.55e3
 r1_init = -r2_init;
 r1dot_init = -r2dot_init;
-G = 6.674e-11; % m^3/(kg s^2)
 
-Tsim = 7*24*3600; % 1 year
+Tsim = 24*3600; % 1 day
 T_sample = 1*3600; % 1 hour
 
-F_G = G*m1*m2/(r2_init(1)-r1_init(1))^2
+F_G = G*m1*m2/((r2_init(1)-r1_init(1))/2)^2
 F_z = m2*r2dot_init(2)^2 / r2_init(1)
 
-% sim('twoBodySim')
+%%
+sim('twoBodySim')
 
-% plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
-%                             'timeScale',T_sample*10)
+plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
+                            'timeScale',T_sample*10)
