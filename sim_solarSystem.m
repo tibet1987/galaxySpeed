8741ae5bcc9abbf53a61fc75374a065e80fdf427
Timo <thundy@web.de>
2020-12-21 16:50:57 +0100

    remove scale factor from calculations in script
    move scale factor only in simulation where it is needed to improve numeriks

--- sim_solarSystem.m
+++ sim_solarSystem.m
@@ -26,7 +26,7 @@ m1 = 2e30; %kg mass of the sun
 m2 = 1;%6e24; % mass of the earth
 scaleFac = 1e6;
 fixPositionOfMass1 = 1;
-r2_init = [149e9/scaleFac 0, 0]';
+r2_init = [149e9 0, 0]';
 r2dot_init = [0, 29.78e3,0]';
 r1_init = -r2_init*1e-6;
 r1dot_init = -r2dot_init*1e-6;
@@ -51,7 +51,7 @@ m1 = 2e30; %kg mass of the sun
 m2 = 1;%6e24; % mass of the earth
 scaleFac = 1e6;
 fixPositionOfMass1 = 1;
-r2_init = [149e9/scaleFac 0, 0]';
+r2_init = [149e9 0, 0]';
 r2dot_init = [0, 29.78e3*0.5,0]';
 r1_init = -r2_init*1e-6;
 r1dot_init = -r2dot_init*1e-6;
@@ -69,12 +69,13 @@ set_param(mdl_settings,'Solver','ode23tb','AbsTol','1e-10','RelTol','1e-10')
 sim('twoBodySim')
 
 plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
-                            'timeScale',T_sample*10000,'trailLength',10)
+                            'timeScale',T_sample*100);%,'trailLength',10)
 
 %% movement of Pluto and Charon
 close all
 m1 = 1.3e22; % kg, mass of body 1 (Pluto equivalent)
 m2 = 1.5e21;   % kg, mass of body 2
+scaleFac = 1e6;
 fixPositionOfMass1 = 0;
 r2_init = [19600 0, 0]';
 r2dot_init = [0, 0.21e3,0]';
@@ -85,30 +86,36 @@ G = 6.674e-11; % m^3/(kg s^2)
 Tsim = 300;%365*24*3600; % 1 year
 T_sample = 1*3600; % 1 hour
 
+load_system('twoBodySim')
+mdl_settings = getActiveConfigSet('twoBodySim');
+set_param(mdl_settings,'Solver','ode23tb','AbsTol','1e-10','RelTol','1e-10')
 sim('twoBodySim')
 
 plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
-                            'timeScale',T_sample*1000)
+                            'timeScale',T_sample*1000,'trailLength',10)
                         
 %% movement of two bodies attacked by gravity
 close all
 G = 6.674e-11; % m^3/(kg s^2)
-m1 = 1e15; % kg, mass of body 1 (Pluto equivalent)
+scaleFac = 1;%1e6;
+m1 = 1e22; % kg, mass of body 1 (Pluto equivalent)
 m2 = m1;   % kg, mass of body 2
 fixPositionOfMass1 = 0;
-r2_init = [5e8 0, 0]';
-r2dot_init = [0, sqrt(G*m1/r2_init(1)),0]'; % 0.55e3
+r2_init = [1e8 0, 0]';
+r2dot_init = [0, sqrt(G*m1/(4*r2_init(1))),0]'; % 0.55e3
 r1_init = -r2_init;
 r1dot_init = -r2dot_init;
 
-Tsim = 24*3600; % 1 day
-T_sample = 1*3600; % 1 hour
+Tsim = 5*356*24*3600; % 5 years
+T_sample = 24*3600; % 1 day
 
-F_G = G*m1*m2/((r2_init(1)-r1_init(1))/2)^2
+F_G = G*m1*m2/(2*(r2_init(1)-r1_init(1)))^2
 F_z = m2*r2dot_init(2)^2 / r2_init(1)
 
-%%
+load_system('twoBodySim')
+mdl_settings = getActiveConfigSet('twoBodySim');
+set_param(mdl_settings,'Solver','ode23tb','AbsTol','1e-10','RelTol','1e-10')
 sim('twoBodySim')
 
-plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',1,...
-                            'timeScale',T_sample*10)
+plotTwoBodySimResults(r1,r2,'size1',30,'size2',18,'plotTrail',0,...
+                            'timeScale',T_sample*1000);%,'trailLength',10)
