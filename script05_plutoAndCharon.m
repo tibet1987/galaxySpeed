96aab847727a134b03eb1542e6f3a67b0e2fffe3
Timo <thundy@web.de>
2020-12-23 18:02:12 +0100

    update docu and try to improve plutCharon
    remove word docu and keep ppt
    try to improve plutCharon simulation

--- script05_plutoAndCharon.m
+++ script05_plutoAndCharon.m
@@ -3,15 +3,19 @@ addpath functions
 
 %% movement of Pluto and Charon
 close all
+G = 6.674e-11; % m^3/(kg s^2)
 m1 = 1.3e22; % kg, mass of body 1 (Pluto equivalent)
 m2 = 1.5e21;   % kg, mass of body 2 (charon equivalent)
 scaleFac = 1e6;
 fixPositionOfMass1 = 0;
 r2_init = [19600 0, 0]';
-r2dot_init = [0, 0.21e3,0]';
-r1_init = -r2_init*m2/m1;
-r1dot_init = -r2dot_init*m2/m1;
-G = 6.674e-11; % m^3/(kg s^2)
+r2dot_init = [0, sqrt(G*m1/(4*r2_init(1))),0]';
+r1_init = -r2_init*m2/(m1+m2);
+r1dot_init = [0, sqrt(G*m2/(4*r2_init(1))),0]';
+
+
+F_G = G*m1*m2/(r2_init(1)-r1_init(1))^2
+F_z = m2*r2dot_init(2)^2 / r2_init(1)
 
 Tsim = 300;%365*24*3600; % 1 year
 T_sample = 1*3600; % 1 hour
