103ed63bdca9743ed1c1108cafb6d6b27c5768d9
Timo <thundy@web.de>
2020-12-25 22:28:07 +0100

    trying to bugfix the calculations for script5

--- script05_plutoAndCharon.m
+++ script05_plutoAndCharon.m
@@ -9,15 +9,15 @@ m2 = 1.5e21;   % kg, mass of body 2 (charon equivalent)
 scaleFac = 1e6;
 fixPositionOfMass1 = 0;
 R_low = 19600e3; % periapsis
-r2_init = [R_low*m1/(m1-m2) 0, 0]';
-r1_init = -m2/m1*r2_init;
-r2dot_init = [0, sqrt( G*m2 / norm(r2_init - r1_init) ),0]';
+r2 = [R_low*m1/(m1+m2) 0, 0]';
+r1 = -m2/m1*r2;
+r2dot = [0, sqrt( norm(r2)*G*m1 / norm(r2 + r1)^2 ),0]';
+r1dot = -m2/m1*r2dot;
 
-r1dot_init = -m2/m1*r2dot_init;
 
-
-F_G = G*m1*m2/(r2_init(1)-r1_init(1))^2
-F_z = m2*r2dot_init(2)^2 / r2_init(1)
+F_G = G*m1*m2 / norm(r2 - r1)^2
+F_z2 = m2*norm(r2dot)^2 / norm(r2)
+F_z1 = m1*norm(r1dot)^2 / norm(r2)
 
 Tsim = 300;%365*24*3600; % 1 year
 T_sample = 1*3600; % 1 hour
